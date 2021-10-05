0d689e47d325b50624d4ecaf7da7dd975b5b36f5
Mohamed Bah <mbah0397@gmail.com>
2021-10-05 18:00:18 +0000

    Crud Spring Boot

---
+++ OdkApprenants/OdkApprenants/src/main/java/com/OdkApprenant/demo/OdkApprenantsApplication.java
@@ -0,0 +1,13 @@
+package com.OdkApprenant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdkApprenantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdkApprenantsApplication.class, args);
+	}
+
+}
